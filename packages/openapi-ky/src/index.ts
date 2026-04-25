6710d022394073ee46a8c354dd7086ad73d9fafd
nijesmik <sejin1398@gmail.com>
2026-04-26 04:24:25 +0900

    ♻️ Params를 public export에 추가

--- packages/openapi-ky/src/index.ts
+++ packages/openapi-ky/src/index.ts
@@ -1,4 +1,4 @@
 export { createClient } from "./client";
 export type { Client } from "./client";
 export { isHTTPError } from "./lib/is-http-error";
-export type { Options, PathsFor, RequestBody, ResponseBody } from "./types";
+export type { Options, Params, PathsFor, RequestBody, ResponseBody } from "./types";

6e429c2305af8f564e3c79dde67fe86200f3c6e0
nijesmik <sejin1398@gmail.com>
2026-04-26 21:30:40 +0900

    ✨ types/index.ts 도입 + JsonField/KyOptions public export

---
+++ packages/openapi-ky/src/types/index.ts
@@ -0,0 +1,5 @@
+export type { Client } from "./client";
+export type { Params, PathsFor, RequestBody, ResponseBody, SearchParams } from "./common";
+export type { JsonField, Options } from "./options";
+export type { HttpMethod } from "openapi-typescript-helpers";
+export type { Options as KyOptions } from "ky";
